da2f9d52c9393572826be660f06a95341c6a4fa0
abressiddique <abressiddique@gmail.com>
2024-09-01 05:29:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './components/todo-list';
+
+function App() {
+  return (
+    <div>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
